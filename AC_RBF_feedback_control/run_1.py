de0f0e1894ddd2d1845f19ccdc24a4583e1a284b
Break!! <52427538+wwsyan@users.noreply.github.com>
2023-05-16 21:38:30 +0800

    Update run_1.py

--- AC_RBF_feedback_control/run_1.py
+++ AC_RBF_feedback_control/run_1.py
@@ -16,13 +16,11 @@ for k in range(K1):
 for k in range(K1,K2):
     y_d[k] = 0.5*np.cos(PI*k*T) + 0.5*np.sin(PI*k*T/2)
 
-
 actor = Actor(n=1)
 critic = Critic()
 
 # 输入参考信号的系统动态
 x = np.zeros((K2, 2))
-y = np.zeros(K2)
 e = np.zeros(K2)
 x[0] = [0, 0.3]
 
@@ -41,12 +39,14 @@ for k in range(K2-1):
     
     # 系统动态
     x[k+1, 0] = 0.2 * np.square(x[k, 0]) * x[k, 1] / (1 + np.square(x[k, 0])) + 0.5 * x[k, 1]
-    x[k+1, 1] = x[k, 0] / (1 + np.square(x[k, 0]) + np.square(x[k, 1])) + u_k + 0.05*np.cos(0.01*k)*np.cos(x[k, 0])
+    x[k+1, 1] = x[k, 0] / (1 + np.square(x[k, 0]) + np.square(x[k, 1])) \
+                + u_k \
+                + 0.2*np.sin(u_k) \
+                + 0.05*np.cos(0.01*k)*np.cos(x[k, 0])
     
-    y[k] = y_k
-    e[k] = e_k
     
 y = x[:, 1]
+e = y - y_d
 
 # 画图
 fig, axs = plt.subplots(3, 1)
@@ -57,7 +57,7 @@ axs[0].set_title("y_d")
 
 axs[1].plot(x)
 axs[1].set_title("States")
-axs[1].legend(["x1", "x2 = y"])
+axs[1].legend(["x1 = y", "x2"])
 
 axs[2].plot(e)
 axs[2].set_title("y - y_d")
